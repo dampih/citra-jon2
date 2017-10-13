b3aaa9258efb622d1c414efa8932bc70207b89a4
2 <2>
2017-10-13 19:01:26 +0100

    N3DS_FCRAM_Error_Correction

--- src/core/memory.h
+++ src/core/memory.h
@@ -115,10 +115,11 @@ enum : PAddr {
 
     /// Main FCRAM
     FCRAM_PADDR = 0x20000000,
-    FCRAM_SIZE = 0x08000000,      ///< FCRAM size on the Old 3DS (128MB)
-    FCRAM_N3DS_SIZE = 0x10000000, ///< FCRAM size on the New 3DS (256MB)
+    FCRAM_SIZE = 0x08000000, ///< FCRAM size on the Old 3DS (128MB)
+    FCRAM_N3DS_PADDR = 0x28000000, ///< Buffing Up the FCRAM N3DS Address
+    FCRAM_N3DS_SIZE = 0x08000000, ///< + 0x28 + 0x08 FCRAM size on the N3DS = (256MB)
     FCRAM_PADDR_END = FCRAM_PADDR + FCRAM_SIZE,
-    FCRAM_N3DS_PADDR_END = FCRAM_PADDR + FCRAM_N3DS_SIZE,
+    FCRAM_N3DS_PADDR_END = FCRAM_PADDR + FCRAM_SIZE + FCRAM_N3DS_SIZE,
 };
 
 /// Virtual user-space memory regions
